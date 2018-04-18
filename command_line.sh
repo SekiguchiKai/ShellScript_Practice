495c540d60072d1fa5b8d95230ccd855302150d2
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-04-18 23:24:11 +0900

    特殊変数を追加

--- command_line.sh
+++ command_line.sh
@@ -5,3 +5,12 @@
 # /command_line.sh Hogeのような感じでコマンドライン引数を指定する
 echo "こんにちわ $1"
 
+# $0 script自体の名前
+# 「このscriptは、{./command_line.sh}です」になる
+echo "このscriptは、{$0}です"
+
+# 引数の数
+echo "引数の数は、{$#}です"
+
+echo "全ての引数パターン1 : {$@}"
+echo "全ての引数パターン1 : {$*}"
