ec6487876778640a86cbbc6a5b8b45677bedc337
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-04-18 23:33:01 +0900

    対話型

---
+++ input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read programming1
+echo "あなたが入力したのは、${programming1}ですね！"
+
+read -p "好きな言語: " programming2
+echo "あなたが好きな言語は、${programming2}ですね！"
+
+read -p "好きな言語を3つ入力してね: " p1 p2 p3
+echo "1位は、${p1}ですね!"
+echo "2位は、${p2}ですね!"
+echo "3位は、${p3}ですね!"
